edb0fb06f5f60795709385ec4f50e390cec36787
Craigharrison79 <craigharrison7917@hotmail.com>
2021-07-25 21:07:37 +0000

    Initial commit, add Procfile and requirements.txt as well as connecting flask to Mongo DB and creating a Flask app

---
+++ app.py
@@ -0,0 +1,27 @@
+import os
+from flask import Flask
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+if os.path.exists("env.py"):
+    import env
+
+
+app = Flask(__name__)
+
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
+
+
+@app.route("/")
+@app.route("/get_meals")
+def get_meals():
+    return "This workings"
+
+
+if __name__ == "__main__":
+    app.run(host=os.environ.get("IP"),
+            port=int(os.environ.get("PORT")),
+            debug=True)  # Change to False before submit the project
